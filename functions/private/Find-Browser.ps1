f040a39e69b0b2a51c748ff4ad9b9cfbd4f1b15c
Trevor Sullivan <trevor@trevorsullivan.net>
2023-03-26 23:55:40 -0600

    Update Find-Browser.ps1

--- functions/private/Find-Browser.ps1
+++ functions/private/Find-Browser.ps1
@@ -7,6 +7,17 @@ function Find-Browser {
   param (
 
   )
+  
+  if ($IsMacOS) {
+    $ChromePath = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    $FirefoxPath = "/Applications/Firefox.app/Contents/MacOS/firefox"
+
+    if (Get-Item -Path $ChromePath) { return $ChromePath }
+    if (Get-Item -Path $FirefoxPath) { return $FirefoxPath }
+
+    throw 'Could not find browser on MacOS'
+    return
+  }
 
   $CommandList = @(
     'chrome.exe'
